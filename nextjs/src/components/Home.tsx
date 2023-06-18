895c6e28fa11f01d88eb0f9d4af40a8bffaaa9ca
Tu <tucq88@gmail.com>
2023-06-18 19:50:44 +0700

    fix: re-enable prompt form

--- nextjs/src/components/Home.tsx
+++ nextjs/src/components/Home.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   const defaultChats: Chat[] = [{ sender: 'bot', msg: "Hello it's ChatBot - Ask me anything!" }]
   const [chats] = useState(defaultChats)
   const [prompt, setPrompt] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const bubbleSideClassName = (sender: string) => {
     return 'chat chat-' + (sender === 'bot' ? 'start' : 'end')
@@ -32,45 +33,52 @@ export default function Home() {
       },
     }
 
+    setLoading(true)
+
     axios
       .post('/api/completion', { prompt: prompt }, axiosConfig)
       .then((response) => {
         chats.push({ sender: 'you', msg: prompt })
         chats.push({ sender: 'bot', msg: response.data.result })
       })
-      .finally(() => setPrompt(''))
+      .finally(() => {
+        setLoading(false)
+        setPrompt('')
+      })
   }
 
   return (
-    <div className="p-8 flex justify-center">
-      <div></div>
-      <section className="basic-1/2">
-        {chats.map((chat, index) => (
-          <div className={bubbleSideClassName(chat.sender)} key={index}>
-            <div className={bubbleColorClassName(chat.sender)}>{chat.msg}</div>
-          </div>
-        ))}
-      </section>
-      <div></div>
+    <>
+      <div className="p-8 flex justify-center">
+        <section className="w-6/12">
+          {chats.map((chat, index) => (
+            <div className={bubbleSideClassName(chat.sender)} key={index}>
+              <div className={bubbleColorClassName(chat.sender)}>{chat.msg}</div>
+            </div>
+          ))}
+        </section>
+      </div>
 
-      {/* <div className="w-6/12">
-        <hr />
-        <form action="#" className="relative" onSubmit={(e) => e.preventDefault()}>
-          <div className="form-control">
-            <br />
-            <textarea
-              className="textarea textarea-bordered h-24"
-              placeholder="Enter your prompt"
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            ></textarea>
-            <br />
-          </div>
-          <button className="btn" onClick={submitPrompt}>
-            Submit
-          </button>
-        </form>
-      </div> */}
-    </div>
+      <div className="p-8 flex justify-center">
+        <section className="w-6/12">
+          <hr className="border-gray-900" />
+          <form action="#" className="relative" onSubmit={(e) => e.preventDefault()}>
+            <div className="form-control">
+              <br />
+              <textarea
+                className="textarea textarea-bordered h-24"
+                placeholder="Enter your prompt"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+              ></textarea>
+              <br />
+            </div>
+            <button className="btn" onClick={submitPrompt} disabled={loading}>
+              {!loading ? 'Submit' : 'Loading...'}
+            </button>
+          </form>
+        </section>
+      </div>
+    </>
   )
 }
